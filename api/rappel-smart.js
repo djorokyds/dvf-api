5146e3ae8c49ecd50f749c7266dd795848592470
djorokyds <djoro.kla@gmail.com>
2026-03-23 19:18:03 +0100

    Update rappel-smart.js

--- api/rappel-smart.js
+++ api/rappel-smart.js
@@ -1,9 +1,16 @@
 module.exports = async function handler(req, res) {
+  const userAgent = req.headers['user-agent'] || '';
+  const isIOS = /iPhone|iPad|iPod/i.test(userAgent);
+
   const googleCalendarUrl = 'https://calendar.google.com/calendar/render?action=TEMPLATE' +
     '&text=%F0%9F%92%B0+Fi-One+%E2%80%94+Saisis+tes+d%C3%A9penses+!' +
     '&details=Ton+budget+du+mois+t%27attend.+2+minutes+suffisent+pour+rester+sur+la+bonne+voie+%F0%9F%8E%AF%0AOuvrir+Fi-One+%3A+https%3A%2F%2Ffi-one.glide.page' +
     '&recur=RRULE%3AFREQ%3DWEEKLY%3BBYDAY%3DMO' +
     '&dates=20260401T190000%2F20260401T191500';
 
-  return res.redirect(302, googleCalendarUrl);
+  if (isIOS) {
+    return res.redirect(302, 'webcal://djorokyds.github.io/dvf-api/rappel.ics');
+  } else {
+    return res.redirect(302, googleCalendarUrl);
+  }
 };
