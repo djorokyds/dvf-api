6e08a80dc4f266ac28ea16fa45913f190fe83457
djorokyds <djoro.kla@gmail.com>
2026-03-23 18:46:04 +0100

    Update rappel-smart.js

--- api/rappel-smart.js
+++ api/rappel-smart.js
@@ -1,13 +1,4 @@
 module.exports = async function handler(req, res) {
-  const userAgent = req.headers['user-agent'] || '';
-  const isIOS = /iPhone|iPad|iPod/i.test(userAgent);
-
-  const googleCalendarUrl = 'https://calendar.google.com/calendar/render?action=TEMPLATE' +
-    '&text=%F0%9F%92%B0+Fi-One+%E2%80%94+Saisis+tes+d%C3%A9penses+!' +
-    '&details=Ton+budget+du+mois+t%27attend.+2+minutes+suffisent+pour+rester+sur+la+bonne+voie+%F0%9F%8E%AF%0AOuvrir+Fi-One+%3A+https%3A%2F%2Ffi-one.glide.page' +
-    '&recur=RRULE%3AFREQ%3DWEEKLY%3BBYDAY%3DMO' +
-    '&dates=20260401T190000%2F20260401T191500';
-
   const icsContent = `BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Fi-One//FR
@@ -28,11 +19,8 @@ END:VALARM
 END:VEVENT
 END:VCALENDAR`;
 
-  if (isIOS) {
-    // webcal:// ouvre directement l'app Calendrier iOS sans passer par Safari/WebView
-    const webcalUrl = 'webcal://dvf-api-flame.vercel.app/api/rappel';
-    return res.redirect(302, webcalUrl);
-  } else {
-    return res.redirect(302, googleCalendarUrl);
-  }
+  res.setHeader('Content-Type', 'text/calendar; charset=utf-8');
+  res.setHeader('Content-Disposition', 'attachment; filename="fi-one-rappel.ics"');
+  res.setHeader('Cache-Control', 'no-cache');
+  return res.status(200).send(icsContent);
 };
