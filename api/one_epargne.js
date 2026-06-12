b8f73ff22933c2013df6a2beeea40a0d7c497019
djorokyds <djoro.kla@gmail.com>
2026-06-12 11:35:45 +0200

    Update one_epargne.js

--- api/one_epargne.js
+++ api/one_epargne.js
@@ -26,11 +26,8 @@ module.exports = async function handler(req, res) {
       width: 100%;
       height: auto;
     }
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     body {
-      font-family: 'Inter', sans-serif;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', sans-serif;
       background: #1f1f1f;
       color: #eaeaea;
       display: flex;
